e79933cb947c489bccf70edb59a132c96d70c6c3
timosii <tsimashev@gmail.com>
2024-04-15 00:02:26 +0300

    feat: structure

---
+++ app/handlers/main.py
@@ -0,0 +1,15 @@
+from aiogram import Dispatcher
+
+from app.handlers.user import register_users_handlers
+from app.handlers.admin import register_admin_handlers
+from app.handlers.other import register_other_handlers
+
+
+def register_all_handlers(dp: Dispatcher) -> None:
+    handlers = (
+        register_admin_handlers,
+        register_users_handlers,
+        register_other_handlers
+    )
+    for handler in handlers:
+        handler(dp)
--- app/main.py
+++ app/main.py
@@ -1,4 +1,36 @@
+from loguru import logger
+from aiogram import Bot, Dispatcher, executor
+from aiogram.fsm.storage.redis import RedisStorage, Redis
+from app.config import settings
+
+
+async def __on_start_up(dp: Dispatcher) -> None:
+    logger.info('Bot starts')
+
+    register_models()
+    register_all_filters(dp)
+    register_all_handlers(dp)
+
+    # users = get_users_with_sessions()
+    # count = 0
+
+    # if not users:
+    #     return
+
+    # for user in users:
+    #     with suppress(ChatNotFound, BotBlocked):
+    #         if user.session.enable:
+    #             start_process_if_sessions_exists(user.telegram_id)
+    #         await dp.bot.send_message(user.telegram_id, "Бот обновлен!",
+    #                                   reply_markup=get_main_keyboard(user.telegram_id))
+    #         count += 1
+
+    # logger.info(f"Было совершено {count} рассылок")
 
 
 
-BOT_TOKEN = settings.BOT_TOKEN
+def start_bot() -> None:
+    bot = Bot(token=settings.BOT_TOKEN, parse_mode='HTML')
+    dp = Dispatcher(bot, storage=RedisStorage(redis=Redis(host='localhost')))
+    executor.start_polling(dp, skip_updates=True, on_startup=__on_start_up)
+
