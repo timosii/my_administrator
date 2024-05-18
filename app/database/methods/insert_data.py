5400ded521f243aefb2e13735fd6bd521522dea5
timosii <tsimashev@gmail.com>
2024-05-18 22:53:02 +0300

    fix: convert date interval process

--- app/database/methods/insert_data.py
+++ app/database/methods/insert_data.py
@@ -7,9 +7,9 @@ from app.database.models.data import (
 )
 
 
-async def insert_data():
-    async with session_maker() as session:
-        user_test = User(user_id='661772')
-        user_test_2 =  User(user_id='2121212')
-        session.add_all([user_test, user_test_2])
-        await session.commit()
+# async def insert_data():
+#     async with session_maker() as session:
+#         user_test = User(user_id='661772')
+#         user_test_2 =  User(user_id='2121212')
+#         session.add_all([user_test, user_test_2])
+#         await session.commit()
