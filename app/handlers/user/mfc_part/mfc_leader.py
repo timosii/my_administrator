45c00b3a34bd11f06aad33fc95d3dec078065b4e
timosii <tsimashev@gmail.com>
2024-08-01 18:19:58 +0300

    finish basic calendar logic

--- app/handlers/user/mfc_part/mfc_leader.py
+++ app/handlers/user/mfc_part/mfc_leader.py
@@ -69,7 +69,7 @@ async def back_command(message: Message, state: FSMContext):
 ##############
 
 
-@router.message(F.text.lower() == 'посмотреть отчет о работе сотрудников мфц', StateFilter(MfcLeaderStates.mfc_leader))
+@router.message(F.text.lower() == 'посмотреть отчет о работе сотрудников мфц')
 async def get_period(message: Message, state: FSMContext):
     await message.answer(
         '<b>Выберите начало периода:</b> ',
@@ -90,41 +90,44 @@ async def start_period_calendar(callback_query: CallbackQuery, callback_data: Ca
             start_period=date.isoformat()
         )
         await callback_query.message.answer(
-            f'Вы выбрали начало периода: {date.strftime("%d-%m-%Y")}\nВыберите конец периода:',
-            reply_markup=await SimpleCalendar().start_calendar()
+            text='<b>Выберите конец периода:</b> ',
+            reply_markup=await SimpleCalendar(selected_date=date).start_calendar()
         )
         await state.set_state(MfcLeaderStates.get_end_period)
 
 
 @router.callback_query(SimpleCalendarCallback.filter(), StateFilter(MfcLeaderStates.get_end_period))
 async def end_period_calendar(callback_query: CallbackQuery, callback_data: CallbackData, state: FSMContext):
+    data = await state.get_data()
+    start_period = dt.datetime.fromisoformat(data['start_period'])
     calendar = SimpleCalendar(
-        show_alerts=True
+        show_alerts=True,
+        selected_date=start_period
     )
     calendar.set_dates_range(dt.datetime(2022, 1, 1), dt.datetime(2025, 12, 31))
     selected, date = await calendar.process_selection(callback_query, callback_data)
-    data = await state.get_data()
     if selected:
         await state.update_data(
             finish_period=date.isoformat()
         )
+       
         await callback_query.message.answer(
-            f"Отчетный период: {dt.datetime.fromisoformat(data['start_period']).strftime('%d-%m-%Y')} -- {date.strftime('%d-%m-%Y')}",
+            text=f"Отчетный период: <b>{dt.datetime.fromisoformat(data['start_period']).strftime('%d-%m-%Y')} - {date.strftime('%d-%m-%Y')}</b>\nНажмите <b>Получить отчет</b> для получения отчета",
             reply_markup=MfcLeaderKeyboards().get_report()
         )
         await state.set_state(MfcLeaderStates.full_period)
 
 
-@router.callback_query(F.text.lower() == 'получить отчет', StateFilter(MfcLeaderStates.full_period))
+@router.message(F.text.lower() == 'получить отчет', StateFilter(MfcLeaderStates.full_period))
 async def get_report(message: Message, state: FSMContext):
     await message.answer(
-        'Здесь будет отправлен отчет',
+        text='Здесь будет отправлен отчет',
         reply_markup=MfcLeaderKeyboards().finish_process()
     )
     await state.set_state(MfcLeaderStates.finish_stage)
 
 
-@router.callback_query(F.text.lower() == 'закончить', StateFilter(MfcLeaderStates.finish_stage))
+@router.message(F.text.lower() == 'закончить', StateFilter(MfcLeaderStates.finish_stage))
 async def finish_report(message: Message, state: FSMContext):
     await message.answer(
         'Спасибо, работа закончена!',
